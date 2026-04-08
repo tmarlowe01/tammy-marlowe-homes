e1cd1eb697cf5c3beb2a46f3c74ac3a7d58b1700
tmarlowe01 <tmarlowe01@gmail.com>
2026-04-08 15:14:09 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,8 @@
-
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial, sans-serif" }}>
+      <h1>Tammy Marlowe Homes</h1>
+      <p>Your Southern Maryland real estate website is being connected.</p>
+    </main>
+  );
+}
